6d1c0bff0c406f128411bae07fdcd05c07655496
RomanShekhovtsov <122684491+RomanShekhovtsov@users.noreply.github.com>
2023-02-26 16:10:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+CONTACTS = {}
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def add_change_comand(comand):
+    comand = comand.split(" ")
+    CONTACTS[comand[1].capitalize()] = comand[2]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def phone_comand(comand):
+    comand = comand.split(" ")
+    phone = CONTACTS[comand[1].capitalize()]
+    return phone
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def show_all_comand(CONTACTS):
+    str_dict = ""
+    for key, value in CONTACTS.items():
+        str_dict += f"{key}: {value} \n"
+    str_dict = str_dict[:-2]
+    return str_dict
+
+
+def input_error(func):
+    try:
+        func()
+    except KeyError:
+        print("Enter user name ")
+    except ValueError:
+        print("Give me name and phone please ")
+    except IndexError:
+        print("Give me name and phone please ")
+
+    return func()
+
+
+@input_error
+def main():
+    while True:
+        comand = input(" ")
+
+        if comand.casefold() == "hello":
+            print("How can I help you?")
+
+        elif "add" in comand.casefold() or "change" in comand.casefold():
+            add_change_comand(comand)
+            print("Contact saved")
+
+        elif "phone" in comand.casefold():
+            print(f"phone {phone_comand(comand)}")
+
+        elif "show all" in comand.casefold():
+            print(f"Your contacts:\n {show_all_comand(CONTACTS)} ")
+
+        if comand.casefold() == "good bye" or comand.casefold() == "close" or comand.casefold() == "exit":
+            print("Good bye!")
+            break
+
+
+if __name__ == "__main__":
+    main
